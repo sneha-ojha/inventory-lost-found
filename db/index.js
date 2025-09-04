f6a02eadc25435b16eff9f2964a343b6a05082fa
sneha <ojha.sneha021@outlook.com>
2025-09-04 18:56:50 +0530

    Update db connection to use DATABASE_URL

--- db/index.js
+++ db/index.js
@@ -1,14 +1,12 @@
 // db/index.js
 const { Pool } = require('pg');
 require('dotenv').config();
-console.log('Attempting to connect to database:', process.env.DB_NAME);
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
